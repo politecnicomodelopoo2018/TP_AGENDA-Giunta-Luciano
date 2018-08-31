bbc51d9cdd3aa62a460865124270a3701c8a7cda
ShairaT <ayelenmaite2000@gmail.com>
2018-08-31 11:18:52 -0300

    El anchor de la pantalla mis datos no sube :)

--- calendar.py
+++ calendar.py
@@ -6,20 +6,48 @@ kivy.require("1.8.0")
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
 from kivy.uix.anchorlayout import AnchorLayout
 
+Builder.load_file('screenmanagerapp.kv')
 
-class Box01(AnchorLayout):
-    def __init__(self):
-        super(Box01, self).__init__()
-        self.anchor_x = "center"
-        self.anchor_y = "center"
+class PantallaGeneral(Screen):
+    pass
 
-class MainApp(App):
-    title = "Agenda, pagina 1"
+class MisDatos(Screen):
+    pass
+
+class Contactos(Screen):
+    pass
+
+class Feriados(Screen):
+    pass
+
+root = ScreenManager()
+root.add_widget(PantallaGeneral(name='PantallaGeneral'))
+root.add_widget(MisDatos(name='Mis Datos'))
+root.add_widget(Contactos(name='Contactos'))
+root.add_widget(Feriados(name='Feriados'))
+
+
+'''
+class PantallaSiguiente(Screen):
+    pass
+class PantallaAnterior(Screen):
+    pass
+
+
+root = ScreenManager()
+
+root.add_widget(PantallaGeneral(name='PantallaPrincipal'))
+#root.add_widget(PantallaSiguiente(name='PantallaSiguiente'))
+#root.add_widget(PantallaAnterior(name='PantallaAnterior'))
+'''
+class ScreenManagerApp(App):
+
     def build(self):
-        return Box01()
+        return root
 
-
-if __name__ == "__main__":
-    MainApp().run()
+if __name__ == '__main__':
+    ScreenManagerApp().run()
