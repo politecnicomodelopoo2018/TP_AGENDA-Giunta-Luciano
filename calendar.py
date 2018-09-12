9e46eb5cf9d8c546d34bfff8286a69baf78c7baf
ShairaT <ayelenmaite2000@gmail.com>
2018-09-12 11:33:52 -0300

    Text input into label

--- calendar.py
+++ calendar.py
@@ -1,7 +1,6 @@
 from kivy import kivy
 from kivy.app import App
-
-
+from DB import DB
 kivy.require("1.8.0")
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
@@ -9,9 +8,12 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 from kivy.uix.button import Button
+from kivy.uix.label import Label
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.properties import StringProperty
+from kivy.uix.textinput import TextInput
 
+DB().SetConection('127.0.0.1', 'root', 'alumno', 'TP2')
 Builder.load_file('screenmanagerapp.kv')
 
 
@@ -19,8 +21,13 @@ class PantallaGeneral(Screen):
     pass
 
 class MisDatos(Screen):
-    pass
-
+    def __init__(self, **kwargs):
+        super(MisDatos, self).__init__(**kwargs)
+        self.ids.label1.text = "MIS DATOS"
+        self.ids.button1.background_normal = "Faraona.jpg"
+        nombre = self.ids.caja.text
+        self.ids.label2.text = nombre
+        print(self.ids.caja.text)
 class Contactos(Screen):
     pass
 
